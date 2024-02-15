7d15b452a7a4f9b9988abe722cfe7503c5a4fbb7
james ballanger <jamesballanger@trentu.ca>
2024-02-15 14:51:00 -0500

    backend: flask wip

---
+++ backend/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+# a POST route as "/login" to allow any user with correct username and password to login
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return "post login"
+    elif request.method == "GET":
+        return "get login"
+
+# a POST route as "/signup" to allow new user to sign up
+@app.route("/signup", methods=["POST"])
+def signup():
+    if request.method == "POST":
+        return "post signup"
+
+# a POST route as "/logout" to allow new user to sign up
+@app.route("/logout", methods=["POST"])
+def logout():
+    if request.method == "POST":
+        return "post logout"
+
+# a GET route as "/users" to read all records from the MySQL Users table
+@app.route("/users", methods=["GET"])
+def get_users():
+    if request.method == "GET":
+        return "get users "
+
+# a GET route as "/products" to read all records from the MySQL Products table
+@app.route("/products", methods=["GET"])
+def get_products():
+    if request.method == "GET":
+        return "get products"
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
